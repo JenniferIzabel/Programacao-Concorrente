8025a62980f96fc2526e080177e6294235bdbcd7
JenniferIzabel <izabeljennifer@gmail.com>
2019-04-30 20:59:32 -0300

    pasta renomeada

---
+++ ProgramacaoConcorrente/src/Topico3_Thread/slide_06/a1_ThreadLambda.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Topico3_Thread.slide_06;
+
+/**
+ *
+ * @author Jennifer
+ * 
+ * 1. Implemente o exemplo anterior usando Lambda Expression.
+ */
+public class a1_ThreadLambda extends Thread {
+    
+    
+    public static void main(String[] args) {
+        new Thread( ()->{
+            System.out.println("Print dentro da thread");
+        }
+        ).start();
+    }
+    
+    
+    
+}
---
+++ ProgramacaoConcorrente/src/Topico3_Thread/slide_17/a2_ExibeFrases.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Topico3_Thread.slide_17;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jennifer
+ *
+ * 2) Faça uma thread Java que realize a leitura de um arquivo texto com frases
+ * e exiba as frases a cada 10 segundos.
+ */
+public class a2_ExibeFrases implements Runnable {
+
+    public static void main(String[] args) {
+        new Thread(new a2_ExibeFrases()).start();
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader("/home/jennifer/Documents/UTFPR/2019-1/Programação Concorrente/Programacao-Concorrente/ProgramacaoConcorrente/src/Topico3_slide_17/texto.txt"));
+            String strLine;
+            
+            while ((strLine = br.readLine()) != null) {//Read File Line By Line
+                System.out.println(strLine);
+                dormir(1000); //10000 = 10s
+           
+            }
+            
+            br.close();
+        } catch (Exception e) {//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+        
+
+    }
+
+    static void dormir(int t) {
+        try {
+            Thread.sleep(t);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(a2_ExibeFrases.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
