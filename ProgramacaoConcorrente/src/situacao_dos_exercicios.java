f380a0e66dd36d137e4e53f4fa907d2ad97ec566
JenniferIzabel <izabeljennifer@gmail.com>
2019-04-30 21:00:22 -0300

    arquivo de estado dos exercicios

---
+++ ProgramacaoConcorrente/src/situacao_dos_exercicios.java
@@ -0,0 +1,37 @@
+/*
+
+Topico 3
+    - slide 6
+        - ex1
+        - ex2
+        - ex3
+    - slide 17
+        -
+    - slide 28
+        -
+
+Topico 4
+    -
+
+Topico 6
+    -
+
+Topico 6a
+    -
+
+Topico 7
+    - Sinalização  = OK
+    - Rendezvous
+    - Mutex
+    - Multiplex
+    - Barreira
+    - Barreira Reusável
+    - Fila
+
+
+
+
+
+
+
+ */
