8404c005c0047c009f805b1fd3e63f857a0e6a2d
JenniferIzabel <izabeljennifer@gmail.com>
2019-06-18 21:24:35 -0300

    executor

---
+++ ProgramacaoConcorrente/src/Topico11_Executor/Ex2/Main.java
@@ -0,0 +1,51 @@
+/*
+ * 2. Fa¸ca um programa que calcule a soma dos elementos de uma matriz
+MxN. Divida o programa em tarefas que somam as linhas. O programa
+deve possibilitar especificar o n´umero de threads para resolver o problema.
+ */
+package Topico11_Executor.Ex2;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jennifer
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int qtdThreads = 2;
+        int matriz[][] = {{1,2},{3,4}};
+        int soma = 0;
+        
+        ExecutorService executorService = Executors.newFixedThreadPool(qtdThreads);
+        
+        
+        for (int i = 0; i < matriz.length; i++) {
+            SomaLinhasMatrizTask maiorValorTask = new SomaLinhasMatrizTask(matriz[i]);
+            Future<Integer> future = executorService.submit(maiorValorTask);
+
+            
+            try {
+                soma = soma + future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        
+        
+        executorService.shutdown();
+        while(! executorService.isTerminated()){
+            //do nothing
+        }
+        System.out.println("Soma: "+ soma);
+        System.out.println("Finished all threads");
+    }
+    
+}
---
+++ ProgramacaoConcorrente/src/Topico11_Executor/Ex2/SomaLinhasMatrizTask.java
@@ -0,0 +1,37 @@
+/*
+ * 2. Fa¸ca um programa que calcule a soma dos elementos de uma matriz
+MxN. Divida o programa em tarefas que somam as linhas. O programa
+deve possibilitar especificar o n´umero de threads para resolver o problema.
+ */
+package Topico11_Executor.Ex2;
+
+import java.util.concurrent.Callable;
+
+/**
+ *
+ * @author jennifer
+ */
+public class SomaLinhasMatrizTask implements Callable<Integer>{
+    private int[] linha;
+
+    public SomaLinhasMatrizTask(int[] linha) {
+        this.linha = linha;
+    }
+    
+    private int soma(){
+        int soma = 0;
+        for (int i = 0; i < linha.length; i++) {
+            soma = soma + linha[i];
+        }
+        return soma;
+    }
+    
+    
+    @Override
+    public Integer call() throws Exception {
+        return soma();
+    }
+
+    
+    
+}
