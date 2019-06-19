ac7ae8d1e1704517a9497fc6fadc9d3e84625092
JenniferIzabel <izabeljennifer@gmail.com>
2019-06-18 21:23:07 -0300

    executor

---
+++ ProgramacaoConcorrente/src/Topico11_Executor/Ex1/Main.java
@@ -0,0 +1,67 @@
+/*
+ *1. Fa¸ca um programa que localize o maior valor em um vetor. Divida o
+programa em tarefas que localizam o maior valor em um segmento do
+vetor. O programa deve possibilitar especificar o n´umero de tarefas e o
+n´umero de threads para resolver o problema.
+ */
+package Topico11_Executor.Ex1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sun.jvm.hotspot.utilities.WorkerThread;
+
+/**
+ *
+ * @author jennifer
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int qtdTasks = 5;
+        int qtdThreads = 2;
+        int lista[] = {45,45,65,50,1151,211,216,216,21,6,8};
+        int outra[] = new int[qtdTasks];
+        
+        ExecutorService executorService = Executors.newFixedThreadPool(qtdThreads);
+        
+        int tamSublista = lista.length / qtdTasks;
+        System.out.println("Tamanho da subLista:"+ tamSublista);
+        int a = 0;
+        int b = tamSublista;
+        
+        for (int i = 0; i < qtdTasks; i++) {
+            MaiorValorTask maiorValorTask = new MaiorValorTask(lista, a, b);
+            Future<Integer> future = executorService.submit(maiorValorTask);
+            a = a + tamSublista;
+            b = b + tamSublista;
+            
+            try {
+                outra[i] = future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        
+        MaiorValorTask maiorValorTask = new MaiorValorTask(outra, 0, qtdTasks);
+        Future<Integer> future = executorService.submit(maiorValorTask);
+        try {
+                System.out.println("Maior: "+ future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+            }
+        
+        executorService.shutdown();
+        while(! executorService.isTerminated()){
+            //do nothing
+        }
+        System.out.println("Finished all threads");
+    }
+    
+}
---
+++ ProgramacaoConcorrente/src/Topico11_Executor/Ex1/MaiorValorTask.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Topico11_Executor.Ex1;
+
+import java.util.concurrent.Callable;
+
+/**
+ *
+ * @author jennifer
+ */
+public class MaiorValorTask implements Callable<Integer>{
+
+    private int lista[];
+    private int maior, inicio, fim;
+    
+    public MaiorValorTask(int[] lista, int inicio, int fim) {
+        this.lista = lista;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+    
+    private int maiorSublista(){
+        int m = this.lista[this.inicio];
+        if(this.fim > lista.length){this.fim = lista.length;}
+        
+        for (int i = this.inicio; i < this.fim; i++) {
+            if(this.lista[i] > m){
+                m = this.lista[i];
+            } 
+        }
+        return m;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return maiorSublista();
+    }
+
+    
+    
+}
