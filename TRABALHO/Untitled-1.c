22ed0a048368cc3995614e4cd8c5f223a046fa04
Jennifer Izabel Rodrigues de Souza <jennifer@MacBook-Pro-de-Jennifer.local>
2019-07-15 11:27:41 -0300

    m

--- TRABALHO/Untitled-1.c
+++ TRABALHO/Untitled-1.c
@@ -1,21 +1,21 @@
-// VERSÃO SÓ COM 1 LEITOR
+-- VERSÃO SÓ COM 1 LEITOR
 
-sem_t mutex_area ; // controla o acesso à área (inicia em 1)
+sem_t mutex_area ; -- controla o acesso à área (inicia em 1)
 
 leitor () {
     while (1) {
-        sem_down (&mutex_area) ; // requer acesso exclusivo à área
-        //... // lê dados da área compartilhada
-        sem_up (&mutex_area) ; // libera o acesso à área
-        //...
+        sem_down (&mutex_area) ; -- requer acesso exclusivo à área
+        --... -- lê dados da área compartilhada
+        sem_up (&mutex_area) ; -- libera o acesso à área
+        --...
     }
 }
 escritor () {
     while (1) {
-        sem_down (&mutex_area) ; // requer acesso exclusivo à área
-        //... // escreve dados na área compartilhada
-        sem_up (&mutex_area) ; // libera o acesso à área
-        //...
+        sem_down (&mutex_area) ; -- requer acesso exclusivo à área
+        --... -- escreve dados na área compartilhada
+        sem_up (&mutex_area) ; -- libera o acesso à área
+        --...
     }
 }
 
@@ -31,35 +31,35 @@ escritor () {
 
 
 
-//VERSÃO MAIS DE UM LEITOR
+--VERSÃO MAIS DE UM LEITOR
 
-sem_t mutex_area ; // controla o acesso à área (inicia em 1)
-int conta_leitores = 0 ; // número de leitores acessando a área
-sem_t mutex_conta ; // controla o acesso ao contador (inicia em 1)
+sem_t mutex_area ; -- controla o acesso à área (inicia em 1)
+int conta_leitores = 0 ; -- número de leitores acessando a área
+sem_t mutex_conta ; -- controla o acesso ao contador (inicia em 1)
 
 leitor () {
     while (1) {
-        sem_down (&mutex_conta) ; // requer acesso exclusivo ao contador
-        conta_leitores++ ; // incrementa contador de leitores
-        if (conta_leitores == 1) // sou o primeiro leitor a entrar?
-            sem_down (&mutex_area) ; // requer acesso à área
-        sem_up (&mutex_conta) ; // libera o contador
+        sem_down (&mutex_conta) ; -- requer acesso exclusivo ao contador
+        conta_leitores++ ; -- incrementa contador de leitores
+        if (conta_leitores == 1) -- sou o primeiro leitor a entrar?
+            sem_down (&mutex_area) ; -- requer acesso à área
+        sem_up (&mutex_conta) ; -- libera o contador
 
-       // ... // lê dados da área compartilhada
+       -- ... -- lê dados da área compartilhada
         
-        sem_down (&mutex_conta) ; // requer acesso exclusivo ao contador
-        conta_leitores-- ; // decrementa contador de leitores
-        if (conta_leitores == 0) // sou o último leitor a sair?
-            sem_up (&mutex_area) ; // libera o acesso à área
-        sem_up (&mutex_conta) ; // libera o contador
-        //...
+        sem_down (&mutex_conta) ; -- requer acesso exclusivo ao contador
+        conta_leitores-- ; -- decrementa contador de leitores
+        if (conta_leitores == 0) -- sou o último leitor a sair?
+            sem_up (&mutex_area) ; -- libera o acesso à área
+        sem_up (&mutex_conta) ; -- libera o contador
+        --...
     }
 }
 escritor () {
     while (1) {
-        sem_down(&mutex_area) ; // requer acesso exclusivo à área
-        //... // escreve dados na área compartilhada
-        sem_up(&mutex_area) ; // libera o acesso à área
-        //...
+        sem_down(&mutex_area) ; -- requer acesso exclusivo à área
+        --... -- escreve dados na área compartilhada
+        sem_up(&mutex_area) ; -- libera o acesso à área
+        --...
     }
 }
